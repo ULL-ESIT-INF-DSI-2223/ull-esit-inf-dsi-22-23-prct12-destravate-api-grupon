3e7e882633cd86416e57afa22f954b2021d42f7e
SMarGa <saulmaga02@gmail.com>
2023-05-14 11:36:14 +0100

    changes to adapt to cyclic

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,7 +1,5 @@
 import { connect } from "mongoose";
 
-const uri = "mongodb://127.0.0.1:27017/destravate-api";
-
 try {
   await connect(process.env.MONGODB_URL!);
   console.log("Conectado al servidor MongoDB");
